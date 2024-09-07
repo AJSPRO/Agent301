12ff4dc6bb93245173ed24f9de02987e01786865
AJSPRO <84224122+AJSPRO@users.noreply.github.com>
2024-09-07 18:04:13 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -47,18 +47,19 @@ def claim_tasks(authorization, username):
         'Content-Type': 'application/json',
         'authorization': authorization.strip(),
         'origin': 'https://telegram.agent301.org',
-        'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'
+        'accept-language': 'en-US,en;q=0.9'
     }
 
     url_get_tasks = 'https://api.agent301.org/getMe'
-    response = requests.post(url_get_tasks, headers=headers)
-
-    if response.status_code == 200:
+    try:
+        response = requests.post(url_get_tasks, headers=headers)
+        response.raise_for_status()
         json_response = response.json()
+
         if json_response.get("ok"):
             result = json_response.get("result", {})
             balance = result.get("balance", 0)
-            print(f"{Fore.LIGHTCYAN_EX}Name :  {Fore.LIGHTGREEN_EX}{username} {Fore.LIGHTCYAN_EX}Balance: {Fore.LIGHTGREEN_EX}{balance}AP")
+            print(f"{Fore.LIGHTCYAN_EX}Name: {Fore.LIGHTGREEN_EX}{username} {Fore.LIGHTCYAN_EX}Balance: {Fore.LIGHTGREEN_EX}{balance} AP")
             print(f"{Fore.LIGHTYELLOW_EX}Processing automatic tasks...\n")
 
             # Print claim time
@@ -92,32 +93,35 @@ def claim_tasks(authorization, username):
                             break
 
                 elif not is_claimed and count >= max_count:
-                
                     print(f"{Fore.LIGHTBLUE_EX} • Claiming task • {title}")
-                    claim_task(headers, task_type, title) 
+                    claim_task(headers, task_type, title)
 
             print(f"{Fore.LIGHTRED_EX} ✔ All tasks claimed")
             return balance
 
         else:
             print(f"{Fore.LIGHTRED_EX}Failed to request tasks. Try again.")
-    else:
-        print(f"{Fore.LIGHTRED_EX}Error: {response.status_code}")
+    except requests.RequestException as e:
+        print(f"{Fore.LIGHTRED_EX}Request failed: {e}")
     return 0
 
 def claim_task(headers, task_type, title):
     url_complete_task = 'https://api.agent301.org/completeTask'
     claim_data = {"type": task_type}
-    response = requests.post(url_complete_task, headers=headers, json=claim_data)
-
-    if response.status_code == 200 and response.json().get("ok"):
-        result = response.json().get("result", {})
-        task_reward = result.get("reward", 0)
-        balance = result.get("balance", 0)
-        print(f"{Fore.LIGHTYELLOW_EX}Task {task_type} - {title} - Reward {Fore.LIGHTYELLOW_EX}{task_reward}AP - Balance now : {Fore.LIGHTGREEN_EX}{balance}AP")
-        return True
-    else:
-        print(f"{Fore.LIGHTRED_EX}Task {task_type} - {title} - Failed to complete!")
+    try:
+        response = requests.post(url_complete_task, headers=headers, json=claim_data)
+        response.raise_for_status()
+        if response.json().get("ok"):
+            result = response.json().get("result", {})
+            task_reward = result.get("reward", 0)
+            balance = result.get("balance", 0)
+            print(f"{Fore.LIGHTYELLOW_EX}Task {task_type} - {title} - Reward {Fore.LIGHTGREEN_EX}{task_reward} AP - Balance now: {Fore.LIGHTGREEN_EX}{balance} AP")
+            return True
+        else:
+            print(f"{Fore.LIGHTRED_EX}Task {task_type} - {title} - Failed to complete!")
+            return False
+    except requests.RequestException as e:
+        print(f"{Fore.LIGHTRED_EX}Request failed: {e}")
         return False
 
 def claim_wheel(authorization, username):
@@ -128,56 +132,75 @@ def claim_wheel(authorization, username):
         'Content-Type': 'application/json',
         'authorization': authorization.strip(),
         'origin': 'https://telegram.agent301.org',
-        'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'
+        'accept-language': 'en-US,en;q=0.9'
     }
 
-    url_get_tasks = 'https://api.agent301.org/wheel/load'
-    response = requests.post(url_get_tasks, headers=headers)
-
-    if response.status_code == 200:
+    url_get_tasks = 'https://api.agent301.org/getMe'
+    try:
+        response = requests.post(url_get_tasks, headers=headers)
+        response.raise_for_status()
         json_response = response.json()
+
+        reward_mapping = {
+            'tc4': '4 TON',
+            'c1000': '1,000 AP',
+            't1': '1 ticket',
+            'nt1': '1 NOT',
+            'nt5': '5 NOT',
+            't3': '3 tickets',
+            'tc1': '0.01 TON',
+            'c10000': '10,000 AP'
+        }
+
         if json_response.get("ok"):
-            tasks = json_response.get("result", {}).get("tasks", {})
-            spin_completed = True
+            result = json_response.get("result", {})
+            tickets = result.get("tickets", 0)
+            print(f"{Fore.LIGHTCYAN_EX}Account {Fore.LIGHTGREEN_EX}{username} {Fore.LIGHTCYAN_EX}Ticket Balance: {Fore.LIGHTGREEN_EX}{tickets}")
 
+            if tickets > 0:
+                print(f"\n{Fore.LIGHTYELLOW_EX}------------------------------------")
+                print(Fore.LIGHTCYAN_EX + "• Spinning the Wheel •\n")  # Title for spinning wheel
+                print(f"\n{Fore.LIGHTYELLOW_EX}------------------------------------")
+            else:
+                print(f"{Fore.LIGHTRED_EX}No tickets\n")
+                return
 
-            print(f"\n{Fore.LIGHTYELLOW_EX}------------------------------------")
-            print(Fore.LIGHTCYAN_EX + "• Claiming Wheel Tasks •\n") 
-            print(f"\n{Fore.LIGHTYELLOW_EX}------------------------------------")
+            while tickets > 0:
+                responsew = requests.post('https://api.agent301.org/wheel/spin', headers=headers)
+                if responsew.status_code == 200:
+                    try:
+                        json_responsew = responsew.json()
+                        resultw = json_responsew.get("result", {})
+                        reward_code = resultw.get("reward", '')
+                        reward = reward_mapping.get(reward_code, reward_code)
+                        print(f'{Fore.LIGHTYELLOW_EX}Won: {Fore.LIGHTGREEN_EX}{reward}')
+                    except json.JSONDecodeError:
+                        print(f"{Fore.LIGHTRED_EX}Error: Server response is not valid JSON.")
+                        print(f"{Fore.LIGHTRED_EX}Server response: {responsew.text}")
+                        break
+                else:
+                    print(f"{Fore.LIGHTRED_EX}Error spinning the wheel: {responsew.status_code}")
+                    print(f"{Fore.LIGHTRED_EX}Server response: {responsew.text}")
+                    break
 
-            for _ in range(5):
-                payload = {'type': 'hour'}
-                requests.post('https://api.agent301.org/wheel/task', headers=headers, json=payload)
-                print(f'{Fore.LIGHTYELLOW_EX}turning wheel')
+                response = requests.post(url_get_tasks, headers=headers)
+                if response.status_code == 200:
+                    json_response = response.json()
+                    if json_response.get("ok"):
+                        result = json_response.get("result", {})
+                        tickets = result.get("tickets", 0)
+                    else:
+                        print(f"{Fore.LIGHTRED_EX}Failed to get updated ticket count. Please try again.")
+                        break
+                else:
+                    print(f"{Fore.LIGHTRED_EX}Error getting updated ticket count: {response.status_code}")
+                    break
 
-            if tasks.get('daily', 1) == 0:
-                payload = {'type': 'daily'}
-                requests.post('https://api.agent301.org/wheel/task', headers=headers, json=payload)
-                print(f'{Fore.LIGHTBLUE_EX}Claiming daily tasks on the wheel')
-
-            if not tasks.get('bird', True):
-                payload = {'type': 'bird'}
-                requests.post('https://api.agent301.org/wheel/task', headers=headers, json=payload)
-                print(f'{Fore.LIGHTBLUE_EX}Claiming bird tasks on the wheel')
-
-            if tasks.get('hour', 0) == 0 and tasks.get('daily', 1) == 0 and tasks.get('bird', True):
-                spin_completed = False
-
-            if not spin_completed:
-                print(f'{Fore.LIGHTRED_EX} ✔ Spin tasks completed. Please wait until tomorrow for the next spin.')
-
-            # Add RGB color for spin reward
-            rgb_colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
-            spin_reward = 500  # Example spin reward value
-            color_index = 0
-            print(f'{Fore.LIGHTYELLOW_EX}Spin reward: {rgb_colors[color_index % len(rgb_colors)]}{spin_reward} AP')
-            color_index += 1
-
-            print(f'{Fore.LIGHTRED_EX} ✔ Spin task completed ')
+            print(f"{Fore.LIGHTRED_EX} ✔ Spin finished!")  # Added message
         else:
-            print(f"{Fore.LIGHTRED_EX}Failed to request wheel tasks. Try again.")
-    else:
-        print(f"{Fore.LIGHTRED_EX}Error: {response.status_code}")
+            print(f"{Fore.LIGHTRED_EX}Failed to get tasks. Please try again.")
+    except requests.RequestException as e:
+        print(f"{Fore.LIGHTRED_EX}Request failed: {e}")
 
 def countdown_timer(seconds):
     rgb_colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
@@ -195,18 +218,15 @@ def countdown_timer(seconds):
         seconds -= 1
         color_index += 1
 
-    print(Fore.LIGHTGREEN_EX + 'Finished waiting!')
+    print(Fore.LIGHTGREEN_EX + 'Waiting: 00:00:00')
 
 def main():
-    clear_console()  # Clear console before starting
     print_banner()
-
-    # Initial options in English
-    print(Fore.LIGHTGREEN_EX + "Choose an option:")
-    print(Fore.LIGHTCYAN_EX + "1. Run tasks only")
-    print(Fore.LIGHTCYAN_EX + "2. Run spins only")
-    print(Fore.LIGHTCYAN_EX + "3. Run all")
-    choice = input(Fore.LIGHTGREEN_EX + "Enter your choice (1/2/3): ")
+    print(Fore.LIGHTCYAN_EX + "Choose an option:")
+    print(Fore.LIGHTGREEN_EX + "1. Claim Tasks")
+    print(Fore.LIGHTGREEN_EX + "2. Spin Wheel")
+    print(Fore.LIGHTGREEN_EX + "3. Claim All")
+    choice = input(Fore.LIGHTMAGENTA_EX + "Enter your choice: ")
 
     auth_data = load_authorizations_with_usernames('query.txt')
     total_balance = 0
@@ -217,44 +237,20 @@ def main():
             username = data['username']
 
             print(f"\n{Fore.LIGHTYELLOW_EX}------------------------------------")
-            print(f"{Fore.LIGHTCYAN_EX}Account {Fore.LIGHTCYAN_EX}No.{Fore.LIGHTCYAN_EX}{account_number}")
+            print(f"{Fore.LIGHTYELLOW_EX}Account {Fore.LIGHTWHITE_EX}#{account_number}")
             print(f"{Fore.LIGHTYELLOW_EX}------------------------------------")
 
-            if choice in ('1', '3'):
-                balance = claim_tasks(authorization, username)
-                total_balance += balance
-
-            if choice in ('2', '3'):
+            if choice == '1':
+                total_balance += claim_tasks(authorization, username)
+            elif choice == '2':
+                claim_wheel(authorization, username)
+            elif choice == '3':
+                total_balance += claim_tasks(authorization, username)
                 claim_wheel(authorization, username)
 
-       
-        print(f"\n{Fore.LIGHTYELLOW_EX}------------------------------------")
-        print(Fore.LIGHTCYAN_EX + f"• All tasks and spins complete •")
-        print(f"\n{Fore.LIGHTYELLOW_EX}------------------------------------")
-        
-        print(f"\n{Fore.LIGHTGREEN_EX}Total Balance for all accounts : {Fore.LIGHTYELLOW_EX}{total_balance}AP")
-        
-        print(Fore.LIGHTRED_EX + f"Buy coffee :")
-        print(Fore.LIGHTYELLOW_EX + "  - ajspro.sol  (Solana)")
-        
-        print(Fore.LIGHTYELLOW_EX + "  - 0xC226c4AF21d95d8CaAf6A7b0e060adBCd51C4C24  (EVM)")
-        
-        print(Fore.LIGHTYELLOW_EX + "  - UQBa5v9pWqP1pgEBaGdSI1dbDUvASSpRwN_N4NLyZGamV8WN  (TON)")
-        
-        
-        
-       
-
-        # Wait for 3 hours
-        countdown_timer(10800)  # 3 hours
-
-        clear_console()
-        print_banner()
-        print(Fore.LIGHTCYAN_EX + "Choose an option :")
-        print(Fore.LIGHTYELLOW_EX + "1. Run tasks only")
-        print(Fore.LIGHTYELLOW_EX + "2. Run spins only")
-        print(Fore.LIGHTYELLOW_EX + "3. Run all")
-        choice = input(Fore.LIGHTCYAN_EX + "Enter your choice (1/2/3): ")
+        print(f"{Fore.LIGHTWHITE_EX}Finished!")
+        print(f"{Fore.LIGHTYELLOW_EX}Total Balance: {Fore.LIGHTWHITE_EX}{total_balance} AP")
+        countdown_timer(28800)  # Wait for 8 hours
 
 if __name__ == "__main__":
     main()
